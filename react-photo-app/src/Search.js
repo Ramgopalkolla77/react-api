90e88ade031c6d85390e5c50111734d671bb30a8
Ramgopalkolla77 <80104689+Ramgopalkolla77@users.noreply.github.com>
2021-05-31 15:58:51 +0530

    first commit

---
+++ react-photo-app/src/Search.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import Unsplash, { toJson } from "unsplash-js"
+
+const unsplash = new Unsplash({
+    accessKey: "1vgOCrX-rjtGE7-_P6mxffch8B9fYIdVUqrmDIZJ-x8",
+  });
+
+export default function SearchPhotos() {
+    const [query, setQuery] = useState("");
+    const [pics, setPics] = useState([]);
+
+    const searchPhotos = async (e) => {
+        e.preventDefault();
+        unsplash.search
+    .photos(query)
+    .then(toJson)
+    .then((json) => {
+    setPics(json.results);
+    });
+};
+   
+
+  return (
+      <>
+   <form className="form" onSubmit={searchPhotos}> 
+        <label className="label" htmlFor="query"> 
+          {" "}
+        </label>
+        <input
+          type="text"
+          name="query"
+          className="input"
+          placeholder={`Find photos`}
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type="submit" className="button">
+          Search
+        </button>
+      </form>
+      <div className="card-list">
+      {pics.map((pic) =>  
+      <div className="card"  key={pic.id}>
+              <img
+                className="card--image"
+                alt={pic.alt_description}
+                src={pic.urls.full}
+                width="50%"
+                height="50%"
+              ></img>
+      </div> )}
+      </div>
+      </>
+  );
+}
